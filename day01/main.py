982c0ddfb36025f434f414e471550f1b394f1311
Floozutter <me@floozutter.site>
2023-11-23 00:35:01 -0800

    clean day01

--- day01/main.py
+++ day01/main.py
@@ -5,11 +5,11 @@ INPUTPATH = "input.txt"
 with open(INPUTPATH) as ifile:
     raw = ifile.read()
 
-elves = (
-    (map(int, elf.split()))
+elf_sums = tuple(
+    sum(map(int, elf.split()))
     for elf in raw.strip().split("\n\n")
 )
-top_three = heapq.nlargest(3, map(sum, elves))
+top_three = heapq.nlargest(3, elf_sums)
 
 print(top_three[0])
 print(sum(top_three))
